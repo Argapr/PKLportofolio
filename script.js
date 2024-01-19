a554eb5e727a4b28f9e79f6131f98f9692329cc1
Arga <argaprataman052@gmail.com>
2024-01-19 11:44:59 +0700

    update terbaru

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ function toggleDropdown() {
     var dropdown = burgerIcon.parentElement;
     dropdown.classList.toggle("active");
   }
-
-  document.getElementById('input').addEventListener('change', () => {
-    if (document.body.className.indexOf('dark') === -1) {
-        document.body.classList.add('dark')
-    } else {
-        document.body.classList.remove('dark')
-    }
-})
